e760f884c73bedd69ef7175ab5f02ddd955d428f
Ajay <93866080+ajaypathange@users.noreply.github.com>
2025-10-06 19:18:12 +0530

    itemsData.js

--- src/data/itemsData.js
+++ src/data/itemsData.js
@@ -1,8 +1,17 @@
-export const itemsData = [
+export const level1Items = [
   { id: "1", name: "🔺 Triangle", type: "shape" },
   { id: "2", name: "🟩 Square", type: "shape" },
   { id: "3", name: "🔵 Circle", type: "shape" },
   { id: "4", name: "1️⃣ One", type: "number" },
   { id: "5", name: "2️⃣ Two", type: "number" },
   { id: "6", name: "3️⃣ Three", type: "number" },
+];
+
+export const level2Items = [
+  { id: "7", name: "🟥 Red", type: "color" },
+  { id: "8", name: "🟩 Green", type: "color" },
+  { id: "9", name: "🟦 Blue", type: "color" },
+  { id: "10", name: "🐶 Dog", type: "animal" },
+  { id: "11", name: "🐱 Cat", type: "animal" },
+  { id: "12", name: "🐰 Rabbit", type: "animal" },
 ];
