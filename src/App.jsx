aefbd4fec991c2ddb275ffb9c3a8ee23952c17ed
Ajay <93866080+ajaypathange@users.noreply.github.com>
2025-10-06 19:20:34 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,9 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <div className="app">
-        <h1> Shape & Number Sorting Game</h1>
-        <h3>Drag each item into the correct box!</h3>
-        <GameBoard />
-      </div>
-    </>
+    <div className="App">
+      <GameBoard />
+    </div>
   );
 }
 
